07cef393f0ee5993cc1d56a56f1aefde0cdc6285
UnderwaterCoder <github.5454457@jungblut.one>
2021-03-25 01:50:30 +0100

    Ground settings

--- blap_main_website_desktop/test/widget_test.dart
+++ blap_main_website_desktop/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:blap_main_website_desktop/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(Blap());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
