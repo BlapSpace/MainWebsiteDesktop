23d77ce7cb5f73b1b219f942f99442d29855ab80
Florian Hartnagel <Florian.Hartnagel@bytabo.de>
2021-03-25 17:11:12 +0100

    Add optional textAlign to normalFontComponent

--- blap_main_website_desktop/lib/components/normalFontComponent.dart
+++ blap_main_website_desktop/lib/components/normalFontComponent.dart
@@ -5,22 +5,25 @@ class NormalFontComponent extends StatefulWidget {
   final String _text;
   final Color _color;
   final double _fontSize;
-  const NormalFontComponent(this._text, this._color, this._fontSize);
+  final TextAlign textAlign;
+  const NormalFontComponent(this._text, this._color, this._fontSize, {this.textAlign = TextAlign.left});
   @override
   _NormalFontComponentState createState() =>
-      _NormalFontComponentState(_text, _color, _fontSize);
+      _NormalFontComponentState(_text, _color, _fontSize, textAlign);
 }
 
 class _NormalFontComponentState extends State<NormalFontComponent> {
   final String _text;
   final Color _color;
   final double _fontSize;
-  _NormalFontComponentState(this._text, this._color, this._fontSize);
+  final TextAlign _textAlign;
+  _NormalFontComponentState(this._text, this._color, this._fontSize, this._textAlign);
   @override
   Widget build(BuildContext context) {
     return Text(
       _text,
       style: GoogleFonts.lexendDeca(color: _color, fontSize: _fontSize),
+      textAlign: _textAlign,
     );
   }
 }
