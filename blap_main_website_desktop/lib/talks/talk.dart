885da4b2dcdaf7de94c312d2b742c6c4c2113783
Florian Hartnagel <Florian.Hartnagel@bytabo.de>
2021-03-25 17:41:44 +0100

    Change Talks Builder

---
+++ blap_main_website_desktop/lib/talks/talk.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import '../components/normalFontComponent.dart';
+
+class TalkWidget extends StatefulWidget {
+  @override
+  _TalkWidgetState createState() => _TalkWidgetState();
+}
+
+class _TalkWidgetState extends State<TalkWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 280,
+      height: 150,
+      decoration: BoxDecoration(
+        color: Color(0xFFFFFFFF),
+        borderRadius: BorderRadius.all(
+            Radius.circular(40.0)
+        ),
+        boxShadow: [BoxShadow(
+          color: Color(0xFFF1F8F5),
+          blurRadius: 60,
+          spreadRadius: 0,
+          offset: Offset(-25, 4),
+        ),],
+      ),
+      child: Center(
+        child: NormalFontComponent(
+          'Hier ist der Inhalt von einem Gespräch.',
+          Color(0xFF181818),
+          15,
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
--- blap_main_website_desktop/lib/talks/talksBuilder.dart
+++ blap_main_website_desktop/lib/talks/talksBuilder.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../components/backgroundContainerComponent.dart';
+import '../components/normalFontComponent.dart';
+import 'talk.dart';
+
 class TalksBuilder extends StatefulWidget {
   @override
   _TalksBuilderState createState() => _TalksBuilderState();
@@ -9,7 +13,39 @@ class _TalksBuilderState extends State<TalksBuilder> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Color(0xA0D2BA),
+      height: 900,
+      width: 300,
+      child: BackgroundContainerComponent(
+        Color(0x38A0D2BA),
+        BoxShadow(
+          color: Color(0xFFF1F8F5),
+          blurRadius: 60,
+          spreadRadius: 0,
+          offset: Offset(-25, 4),
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(height: 45),
+            Padding(
+              padding: EdgeInsets.only(left: 55),
+              child: NormalFontComponent('GESPRÄCHE', Color(0xFF181818), 25),
+            ),
+            SizedBox(height: 45),
+            Padding(
+              padding: EdgeInsets.only(left: 10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  TalkWidget(),
+                  SizedBox(height: 10),
+                  TalkWidget(),
+                ],
+              )
+            )
+          ],
+        ),
+      ),
     );
   }
 }
