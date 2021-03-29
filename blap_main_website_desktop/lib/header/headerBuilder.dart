5d63df4eb12119c41893629fa18349b32a1daf1f
Florian Hartnagel <Florian.Hartnagel@bytabo.de>
2021-03-29 16:31:26 +0200

    Add LayoutBuilder to HeaderBuilder

--- blap_main_website_desktop/lib/header/headerBuilder.dart
+++ blap_main_website_desktop/lib/header/headerBuilder.dart
@@ -9,44 +9,48 @@ class HeaderBuilder extends StatefulWidget {
 class _HeaderBuilderState extends State<HeaderBuilder> {
   @override
   Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.topCenter,
-      child: Column(
-        children: <Widget>[
-          SizedBox(
-            height: 20,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                height: 80,
-                width: 800,
-                decoration: BoxDecoration(
-                  color: Color(0x38A0D2BA),
-                  borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(50),
-                    topLeft: Radius.circular(50),
-                    bottomRight: Radius.circular(50),
+    return LayoutBuilder(builder: (BuildContext builderContext, BoxConstraints boxConstraints) {
+      double _heightFactor = boxConstraints.maxHeight / 1024;
+      double _widthFactor = boxConstraints.maxWidth / 1440;
+      return Align(
+        alignment: Alignment.topCenter,
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 20 * _heightFactor,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  height: 80 * _heightFactor,
+                  width: 800 * _widthFactor,
+                  decoration: BoxDecoration(
+                    color: Color(0x38A0D2BA),
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(50),
+                      topLeft: Radius.circular(50),
+                      bottomRight: Radius.circular(50),
+                    ),
+                  ),
+                  child: TextField(
+                    style: GoogleFonts.lexendDeca(
+                        color: Color(0xFF181818), fontSize: 30 * _heightFactor),
+                    cursorColor: Colors.black,
+                    textAlign: TextAlign.start,
+                    decoration: InputDecoration(
+                        hintStyle: GoogleFonts.lexendDeca(
+                            color: Color(0x61000000), fontSize: 30 * _heightFactor),
+                        contentPadding: EdgeInsets.only(left: 30 * _widthFactor, top: 20 * _heightFactor),
+                        border: InputBorder.none,
+                        hintText: "Suche nach Menschen, Story's,..."),
                   ),
                 ),
-                child: TextField(
-                  style: GoogleFonts.lexendDeca(
-                      color: Color(0xFF181818), fontSize: 30),
-                  cursorColor: Colors.black,
-                  textAlign: TextAlign.start,
-                  decoration: InputDecoration(
-                      hintStyle: GoogleFonts.lexendDeca(
-                          color: Color(0x61000000), fontSize: 30),
-                      contentPadding: EdgeInsets.only(left: 30, top: 20),
-                      border: InputBorder.none,
-                      hintText: "Suche nach Menschen, Story's,..."),
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
+              ],
+            ),
+          ],
+        ),
+      );
+    });
   }
 }
